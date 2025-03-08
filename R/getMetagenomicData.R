45810cfc4973ce58383b940e2f5046f4843c8dfd
krlong68 <krlong586@gmail.com>
2025-03-07 19:43:18 -0800

    Convert combine_all.sh to additional draft functions in getMetagenomicData.R Issues: #4, #5

--- R/getMetagenomicData.R
+++ R/getMetagenomicData.R
@@ -1,10 +1,10 @@
-
+#### Download raw output files from cloud storage
 # set up file caching
 # set up google cloud project/bucket access: gs://metagenomics-mac
 
 get_locator <- function(uuid, data_type) {
     # check that data_type %in% c("bugs", "viruses", "unknown")
-    # add ifelse for multiple selection
+    # add ifelse for multiple selection, "all" option
     # allow vector input?
 
     locator <- paste0("gs://metagenomics-mac/results/cMDv4/",
@@ -16,3 +16,19 @@ get_locator <- function(uuid, data_type) {
 }
 
 # find package or use API to cp locator to cache location
+# extract downloaded file
+####
+
+#### Load and format downloaded file
+
+format_metaphlan <- function(uuid, data_type) {
+    # check that data_type %in% c("bugs", "viruses", "unknown")
+    # load raw file from cache
+
+    if (data_type %in% c("bugs", "unkown")) {
+        # convert to SummarizedExperiment
+    } else if (data_type == "viruses") {
+        # convert to SummarizedExperiment (different raw format)
+    }
+}
+####
