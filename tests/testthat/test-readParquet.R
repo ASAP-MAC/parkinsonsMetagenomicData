99eafe96ffc0e52acbbc28c9e9c27969735ea0f3
Levi Waldron <lwaldron.research@gmail.com>
2026-03-20 14:00:52 -0400

    test: harden parquet tests with local fixtures and explicit UUID expectations

--- tests/testthat/test-readParquet.R
+++ tests/testthat/test-readParquet.R
@@ -18,6 +18,7 @@ test_that("httpfs installed", {
 
 ## view_parquet
 test_that("view_parquet creates a single view", {
+    testthat::skip_if_offline()
     con <- db_connect(":memory:")
     view_parquet(con,
                  httpfs_url = "hf://datasets/waldronlab/metagenomics_mac/relative_abundance_uuid.parquet",
@@ -29,6 +30,7 @@ test_that("view_parquet creates a single view", {
 
 ## retrieve_views
 test_that("retrieve_views creates expected views", {
+    testthat::skip_if_offline()
     con <- db_connect(":memory:")
     retrieve_views(con, data_types = "relative_abundance") |>
         suppressMessages()
@@ -92,11 +94,24 @@ test_that("interpret_and_filter applies uuid filtering correctly", {
     expect_equal(res$val, 10)
 })
 
-## parquet_to_tse - humann
-con <- accessParquetData(repo = "waldronlab/metagenomics_mac_examples",
-                         data_types = "pathabundance_unstratified")
-parquet_tbl <- tbl(con, "pathabundance_unstratified_uuid") |> collect()
-tse_basic <- parquet_to_tse(parquet_tbl, data_type = "pathabundance_unstratified")
+## Shared local parquet fixtures
+local_pathcoverage_files <- c(
+    file.path(system.file("extdata", package = "parkinsonsMetagenomicData"),
+              "pathcoverage_unstratified_uuid.parquet"),
+    file.path(system.file("extdata", package = "parkinsonsMetagenomicData"),
+              "pathcoverage_unstratified_pathway.parquet")
+)
+
+local_sample_table_path <- file.path(
+    system.file("extdata", package = "parkinsonsMetagenomicData"),
+    "sample_table.parquet"
+)
+
+## parquet_to_tse - local pathcoverage
+con <- accessParquetData(local_files = local_pathcoverage_files,
+                         data_types = "pathcoverage_unstratified")
+parquet_tbl <- tbl(con, "pathcoverage_unstratified_uuid") |> collect()
+tse_basic <- parquet_to_tse(parquet_tbl, data_type = "pathcoverage_unstratified")
 
 test_that("parquet to TreeSummarizedExperiment (parquet_to_tse())", {
     expect_true(inherits(tse_basic, "TreeSummarizedExperiment"))
@@ -110,49 +125,37 @@ test_that("TSE assays are matrices", {
     expect_true(all(sapply(SummarizedExperiment::assays(tse_basic), function(x) inherits(x, "matrix"))))
 })
 
-## parquet_to_tse - metaphlan
-con <- accessParquetData(repo = "waldronlab/metagenomics_mac_examples",
-                         data_types = "relative_abundance")
-parquet_tbl <- tbl(con, "relative_abundance_uuid") |> collect()
-tse_basic <- parquet_to_tse(parquet_tbl, data_type = "relative_abundance")
-
-test_that("parquet to TreeSummarizedExperiment with MetaPhlAn data", {
-    expect_true(inherits(tse_basic, "TreeSummarizedExperiment"))
-})
-
-test_that("TSE assays don't have NAs with MetaPhlAn data", {
-    expect_true(all(sapply(SummarizedExperiment::assays(tse_basic), function(x) all(!is.na(x)))))
-})
-
-test_that("TSE assays are matrices with MetaPhlAn data", {
-    expect_true(all(sapply(SummarizedExperiment::assays(tse_basic), function(x) inherits(x, "matrix"))))
-})
-
 ## accessParquetData
 test_that("accessParquetData returns a valid DuckDB connection", {
-    con <- accessParquetData(data_types = "pathcoverage_unstratified")
+    con <- accessParquetData(local_files = local_pathcoverage_files,
+                             data_types = "pathcoverage_unstratified")
 
     expect_s4_class(con, "duckdb_connection")
 })
 
 test_that("accessParquetData creates expected views", {
-    con <- accessParquetData(data_types = "pathcoverage_unstratified")
+    con <- accessParquetData(local_files = local_pathcoverage_files,
+                             data_types = "pathcoverage_unstratified")
     views <- DBI::dbListTables(con)
 
-    expect_true(all(grepl("pathcoverage_unstratified", views)))
+    expect_true("pathcoverage_unstratified_uuid" %in% views)
+    expect_true("pathcoverage_unstratified_pathway" %in% views)
 })
 
 ## loadParquetData
 data_type <- "pathcoverage_unstratified"
-con <- accessParquetData(repo = "waldronlab/metagenomics_mac_examples",
+con <- accessParquetData(local_files = local_pathcoverage_files,
                          data_types = data_type)
 
-uuids <- c("8793b1dc-3ba1-4591-82b8-4297adcfa1d7",
-           "cc1f30a0-45d9-41b1-b592-7d0892919ee7",
-           "fb7e8210-002a-4554-b265-873c4003e25f",
-           "d9cc81ea-c39e-46a6-a6f9-eb5584b87706",
-           "4985aa08-6138-4146-8ae3-952716575395",
-           "8eb9f7ae-88c2-44e5-967e-fe7f6090c7af")
+# UUIDs from inst/extdata/pathcoverage_unstratified_uuid.parquet fixture
+uuids <- c(
+    "0a73759e-825f-4276-9348-66fb6a6e2f86",
+    "38d449c8-1462-4d30-ba87-d032d95942ce",
+    "4985aa08-6138-4146-8ae3-952716575395",
+    "5f8d4254-7653-46e3-814e-ed72cdfcb4d0",
+    "6821bf5f-ad59-4204-9d78-9cf9cac97329",
+    "8793b1dc-3ba1-4591-82b8-4297adcfa1d7"
+)
 custom_filter <- tbl(con, "pathcoverage_unstratified_pathway") |>
                filter(grepl("UMP biosynthesis", pathway))
 
@@ -187,38 +190,47 @@ test_that("sampleMetadata was added", {
 })
 
 ## returnSamples
-data("sampleMetadata", package = "parkinsonsMetagenomicData",
-     envir = environment())
-human_samples <- sampleMetadata |>
-    dplyr::filter(control %in% c("Case", "Study Control") &
-                  age >= 16 &
-                  is.na(sex) != TRUE) |>
-    dplyr::filter(host_species == "Homo sapiens") |>
-    head(10)
+# UUIDs from inst/extdata/pathcoverage_unstratified_uuid.parquet fixture
+sample_data <- data.frame(uuid = c(
+    "0a73759e-825f-4276-9348-66fb6a6e2f86",
+    "38d449c8-1462-4d30-ba87-d032d95942ce",
+    "4985aa08-6138-4146-8ae3-952716575395",
+    "5f8d4254-7653-46e3-814e-ed72cdfcb4d0",
+    "6821bf5f-ad59-4204-9d78-9cf9cac97329",
+    "8793b1dc-3ba1-4591-82b8-4297adcfa1d7",
+    "00000000-0000-0000-0000-000000000001"  # Non-existent UUID for testing
+))
 
-mouse_samples <- sampleMetadata |>
-    dplyr::filter(control %in% c("Case", "Study Control") &
-                  age >= 16 &
-                  is.na(sex) != TRUE) |>
-    dplyr::filter(host_species == "Mus musculus") |>
-    head(10)
+pathway_ref <- load_ref(
+    "pathway_ref",
+    file_path = file.path(
+        system.file("extdata", package = "parkinsonsMetagenomicData"),
+        "pathway_ref.parquet"
+    )
+)
 
-sample_data <- rbind(human_samples, mouse_samples)
+feature_data_pathway <- pathway_ref |>
+    dplyr::distinct(pathway_uniref) |>
+    head(5) |>
+    dplyr::rename(pathway = pathway_uniref)
 
-clade_name_ref <- load_ref("clade_name_ref")
-feature_data_genus <- clade_name_ref |>
-    dplyr::filter(grepl("Faecalibacterium", clade_name_genus))
-genus_ex <- returnSamples(data_type = "relative_abundance",
+genus_ex <- returnSamples(data_type = "pathcoverage_unstratified",
                           sample_data = sample_data,
-                          feature_data = feature_data_genus,
+                          feature_data = feature_data_pathway,
+                          repo = NULL,
+                          local_files = local_pathcoverage_files,
                           include_empty_samples = FALSE)
-ex_empty <- returnSamples(data_type = "relative_abundance",
+ex_empty <- returnSamples(data_type = "pathcoverage_unstratified",
                           sample_data = sample_data,
-                          feature_data = feature_data_genus,
+                          feature_data = feature_data_pathway,
+                          repo = NULL,
+                          local_files = local_pathcoverage_files,
                           include_empty_samples = TRUE)
-ex_dry <- returnSamples(data_type = "relative_abundance",
+ex_dry <- returnSamples(data_type = "pathcoverage_unstratified",
                         sample_data = sample_data,
-                        feature_data = feature_data_genus,
+                        feature_data = feature_data_pathway,
+                        repo = NULL,
+                        local_files = local_pathcoverage_files,
                         dry_run = TRUE)
 
 test_that("returnSamples returns TreeSummarizedExperiment", {
@@ -226,16 +238,16 @@ test_that("returnSamples returns TreeSummarizedExperiment", {
 })
 
 test_that("Expected samples were returned, ignoring empty ones", {
-    expect_in(colnames(genus_ex), sample_data$uuid)
-    expect_false(all(sample_data$uuid %in% colnames(genus_ex)))
+    expect_true(all(colnames(genus_ex) %in% sample_data$uuid))
+    expect_false("00000000-0000-0000-0000-000000000001" %in% colnames(genus_ex))
 })
 
 test_that("Expected samples were returned, including empty ones", {
-    expect_setequal(colnames(ex_empty), sample_data$uuid)
+    expect_true("00000000-0000-0000-0000-000000000001" %in% colnames(ex_empty))
 })
 
 test_that("Expected features were returned", {
-    expect_in(rownames(genus_ex), feature_data_genus$clade_name)
+    expect_true(all(rownames(genus_ex) %in% feature_data_pathway$pathway))
 })
 
 test_that("dry_run returns tbl_duckdb_connection", {
@@ -269,29 +281,29 @@ test_that("dry_run returns tbl_duckdb_connection", {
 
 ## get_cdata_only
 test_that("get_cdata_only retrieves requested samples", {
-    con <- accessParquetData(data_types = "relative_abundance")
-    uuids <- c("c3eb1e35-9a43-413d-8078-6a0a7ac064ba",
-               "a82385f0-d1be-4d79-854c-a7fbfe4473e1",
-               "a1444b37-d568-4575-a5c4-14c5eb2a5b89",
-               "2a497dd7-b974-4f04-9e1f-16430c678f06",
-               "496a2d0c-75ae-430f-b969-b15dedc16b3c",
-               "4a786fd8-782f-4d43-937a-36b98e9c0ab6",
-               "c789b8bc-ebe6-4b85-83e1-5cf1bbfa6111",
-               "d311d028-a54b-4557-970c-eb5b77ec0050",
-               "373a5ac4-161a-46c6-b7d8-4f28b854b386",
-               "5a93179f-7ca7-41d8-96d7-dbed215894aa",
-               "7a2e961b-2f13-4760-9392-c896c54e7ec3",
-               "c6ecc460-33db-4032-9092-9148a134f5dc",
-               "e4a83901-e130-4b64-9e7a-fea55bc5f3f2",
-               "6a034c9f-f7c9-4ead-812b-123ee99b1e0b",
-               "ee26b6f0-89fd-45d0-8af9-bc1d9647a700")
-    cdata <- get_cdata_only(con, data_type = "relative_abundance", uuids)
+    con <- accessParquetData(local_files = local_pathcoverage_files,
+                             data_types = "pathcoverage_unstratified")
+    # UUIDs from inst/extdata/pathcoverage_unstratified_uuid.parquet fixture
+    uuids <- c(
+        "0a73759e-825f-4276-9348-66fb6a6e2f86",
+        "38d449c8-1462-4d30-ba87-d032d95942ce",
+        "4985aa08-6138-4146-8ae3-952716575395",
+        "5f8d4254-7653-46e3-814e-ed72cdfcb4d0",
+        "6821bf5f-ad59-4204-9d78-9cf9cac97329",
+        "8793b1dc-3ba1-4591-82b8-4297adcfa1d7",
+        "8eb9f7ae-88c2-44e5-967e-fe7f6090c7af",
+        "cc1f30a0-45d9-41b1-b592-7d0892919ee7",
+        "d9cc81ea-c39e-46a6-a6f9-eb5584b87706",
+        "fb7e8210-002a-4554-b265-873c4003e25f"
+    )
+    cdata <- get_cdata_only(con, data_type = "pathcoverage_unstratified", uuids)
 
     expect_setequal(uuids, cdata$uuid)
 })
 
 ## get_hf_parquet_urls
 test_that("get_hf_parquet_urls pulls correct columns", {
+    testthat::skip_if_offline()
     func_cols <- colnames(get_hf_parquet_urls())
     expect_cols <- c("filename", "url", "data_type", "tool", "description",
                      "units_normalization")
@@ -300,15 +312,20 @@ test_that("get_hf_parquet_urls pulls correct columns", {
 })
 
 ## load_ref
-cn_ref <- load_ref("clade_name_ref")
+cn_ref <- load_ref(
+    "pathway_ref",
+    file_path = file.path(system.file("extdata",
+                                      package = "parkinsonsMetagenomicData"),
+                          "pathway_ref.parquet")
+)
 
 test_that("load_ref returns a table", {
     expect_true(inherits(cn_ref, "tbl"))
 })
 
 test_that("load_ref returns expected columns", {
-    ex_cols <- parquet_colinfo("relative_abundance") |>
-        dplyr::filter(ref_file == "clade_name_ref") |>
+    ex_cols <- parquet_colinfo("pathcoverage_unstratified") |>
+        dplyr::filter(ref_file == "pathway_ref") |>
         dplyr::pull(col_name)
 
     expect_setequal(ex_cols, colnames(cn_ref))
